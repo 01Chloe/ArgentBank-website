d718e1fc695b6da21feaa27d447262f33e6d2761
Chloe Allier <allier.chloe@gmail.com>
2024-02-13 11:10:31 +0100

    Integration page de connexion

--- front/src/pages/Login.js
+++ front/src/pages/Login.js
@@ -1,3 +1,30 @@
+import "../styles/login.css"
+import { Link } from "react-router-dom"
+
 export default function Login() {
-  return <div>Login</div>
+  return (
+    <main className="main bg-dark">
+      <section className="sign-in-content">
+        <i className="fa-solid fa-circle-user"></i>
+        <h1>Sign In</h1>
+        <form>
+          <div className="input-wrapper">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input type="text" id="password" />
+          </div>
+          <div className="input-remember">
+            <input type="checkbox" id="remember-me" />
+            <label htmlFor="remember-me">Remember me</label>
+          </div>
+          <Link to="/User">
+            <button className="sign-in-button">Sign In</button>
+          </Link>
+        </form>
+      </section>
+    </main>
+  )
 }
